fa2b710ad960aed382548ff87f23e5fda2fab545
kanishk.tiwari <87194591+nikithm@users.noreply.github.com>
2021-08-27 00:50:14 +0530

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,14 +1,16 @@
 from kafka import KafkaConsumer
 
+folderName = "keys/"
+
 consumer = KafkaConsumer(
-    "demo-topic",
-    bootstrap_servers="getting-started-with-kafka.htn-aiven-demo.aivencloud.com:17705",
+    "test-topic",
+    bootstrap_servers="kafka-31ed45d2-nikithbhee-1d6c.aivencloud.com:24572",
     client_id="demo-client-1",
     group_id="demo-group",
     security_protocol="SSL",
-    ssl_cafile="ca.crt",
-    ssl_certfile="client.crt",
-    ssl_keyfile="client.key",
+    ssl_cafile=folderName+"ca.pem",
+    ssl_certfile=folderName+"service.cert",
+    ssl_keyfile=folderName+"service.key"
 )
 
 for msg in consumer:
